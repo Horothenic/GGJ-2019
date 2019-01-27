2f7925d3ed3bc0d8483be3c60c5b53850aecd8e1
Ivan Herrera <ivan.herrera.m@outlook.com>
2019-01-26 19:41:52 -0600

    Labels of gameObjects are shown on Scene window

---
+++ GGJ 2019/Assets/_MAIN ASSETS/_GENERAL/_UTILITIES/Inspector/Editor/LabelHandle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameObject))]
+public class LabelHandle : Editor
+{
+    [DrawGizmo(GizmoType.InSelectionHierarchy | GizmoType.NotInSelectionHierarchy)]
+    static void DrawGameObjectName(Transform transform, GizmoType gizmoType)
+    {   
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 16;
+        style.normal.textColor = Color.black;
+
+        if (transform.gameObject.GetComponent<NameHandle>() != null)
+            Handles.Label(transform.position, transform.gameObject.name, style);
+}
+}
---
+++ GGJ 2019/Assets/_MAIN ASSETS/_GENERAL/_UTILITIES/Inspector/NameHandle.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class NameHandle : MonoBehaviour
+{
+    
+}
